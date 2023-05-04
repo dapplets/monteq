2d07ea26cad82c4b37e420abed4607266728d0d6
Alexander Sakhaev <alsakhaev@gmail.com>
2023-05-04 13:58:16 +0700

    feat: qr code scanner with GMS

---
+++ packages/mobile/src/modules/BarcodeScannerModule.ts
@@ -0,0 +1,5 @@
+import {NativeModules} from 'react-native';
+
+const {BarcodeScannerModule} = NativeModules;
+
+export default BarcodeScannerModule;
--- packages/mobile/src/screens/InfoScreen.tsx
+++ packages/mobile/src/screens/InfoScreen.tsx
@@ -1,8 +1,9 @@
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import {useWeb3Modal} from '@web3modal/react-native';
 import * as React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, Alert} from 'react-native';
 import {type RootStackParamList} from '../App';
+import BarcodeScannerModule from '../modules/BarcodeScannerModule';
 
 const InfoScreen = () => {
   const {provider} = useWeb3Modal();
@@ -16,11 +17,23 @@ const InfoScreen = () => {
     navigation.navigate('CameraScreen');
   }
 
+  async function handleGmsScanPress() {
+    try {
+      const data = await BarcodeScannerModule.scan();
+      Alert.alert('Success', data);
+    } catch (e) {
+      // ToDo: catch CANCELED and FAILURE cases
+      console.error(e);
+      Alert.alert('Failure or canceled');
+    }
+  }
+
   return (
     <View>
       <Text>Logged in</Text>
       <Button onPress={handleDisconnectPress} title="Disconnect" />
-      <Button onPress={handleScanPress} title="Scan" />
+      <Button onPress={handleScanPress} title="Scan via third-party lib" />
+      <Button onPress={handleGmsScanPress} title="Scan via gms (preferred)" />
     </View>
   );
 };
