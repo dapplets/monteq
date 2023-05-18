dcee8956c6f34748c976c79639aef46e42fb142e
Alexander Sakhaev <alsakhaev@gmail.com>
2023-05-19 02:44:30 +0700

    feat: use new contract

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -17,7 +17,7 @@ const INFURA_API_KEY = process.env.INFURA_KEY
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || ''
 
 const config: HardhatUserConfig = {
-    solidity: '0.8.20',
+    solidity: '0.8.19', // The contract built with 0.8.20 uploads to Gnosis Chain with the error: ProviderError: BadInstruction
     networks: {
         sepolia: {
             url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
