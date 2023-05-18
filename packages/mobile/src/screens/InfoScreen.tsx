620f6e6fb3efb83c6ba158704dff3c2d2d18d1df
Alexander Sakhaev <alsakhaev@gmail.com>
2023-05-19 04:12:04 +0700

    fix: share button doesn't appear when empty history

--- packages/mobile/src/screens/InfoScreen.tsx
+++ packages/mobile/src/screens/InfoScreen.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   FlatList,
   View,
@@ -9,22 +9,22 @@ import {
   TouchableHighlight,
   Platform,
   TextInput,
-} from 'react-native';
-import Navigation from '../components/Navigation';
-import Title from '../components/TitlePage';
-import {useMonteqContract} from '../contexts/MonteqContractContext';
-import HistoryPay from '../components/HistoryPay';
-import GeneralPayInfo from '../components/GeneralPayInfo';
+} from "react-native";
+import Navigation from "../components/Navigation";
+import Title from "../components/TitlePage";
+import { useMonteqContract } from "../contexts/MonteqContractContext";
+import HistoryPay from "../components/HistoryPay";
+import GeneralPayInfo from "../components/GeneralPayInfo";
 import {
   BASE_CRYPTO_CURRENCY,
   BASE_CRYPTO_MAX_DIGITS,
-} from '../common/constants';
-import {truncate} from '../common/helpers';
-import {useIsFocused} from '@react-navigation/native';
-import {FontFamily} from '../GlobalStyles';
-import {useState} from 'react';
-import ShareModal from '../components/ShareModal';
-import {useUserName} from '../hooks/useUserName';
+} from "../common/constants";
+import { truncate } from "../common/helpers";
+import { useIsFocused } from "@react-navigation/native";
+import { FontFamily } from "../GlobalStyles";
+import { useState } from "react";
+import ShareModal from "../components/ShareModal";
+import { useUserName } from "../hooks/useUserName";
 
 const InfoScreen = () => {
   const isFocused = useIsFocused();
@@ -36,7 +36,7 @@ const InfoScreen = () => {
     loadMoreOutHistory,
     isOutHistoryLoading,
   } = useMonteqContract();
-  const {userName, changeUserName} = useUserName();
+  const { userName, changeUserName } = useUserName();
 
   const [isModalShareVisible, setIsModalShareVisible] = useState(false);
 
@@ -65,32 +65,19 @@ const InfoScreen = () => {
     <>
       <View style={styles.InfoScreenWrapper}>
         {!isOutHistoryLoading && outHistory.length === 0 ? (
-          <View style={styles.CenterContentWrapper}>
-            <Image
-              resizeMode="contain"
-              style={styles.BusinessImg}
-              source={require('../assets/Lines.png')}
-            />
-            <Text style={styles.DescriptionText}>
-              No history of outgoing transactions associated to your wallet
-              right now.
-            </Text>
-          </View>
-        ) : null}
-
-        {outHistory.length > 0 ? (
           <>
             <View style={styles.wrapperTitle}>
               <Title label="Payment history" />
               <TouchableHighlight
-                underlayColor={'transparent'}
+                underlayColor={"transparent"}
                 onPress={openShareModal}
                 activeOpacity={0.5}
-                style={styles.share}>
+                style={styles.share}
+              >
                 <Image
                   style={styles.shareImg}
                   resizeMode="contain"
-                  source={require('../assets/share.png')}
+                  source={require("../assets/share.png")}
                 />
               </TouchableHighlight>
             </View>
@@ -107,22 +94,69 @@ const InfoScreen = () => {
               <Image
                 style={styles.shareImg}
                 resizeMode="contain"
-                source={require('../assets/edit.png')}
+                source={require("../assets/edit.png")}
+              />
+            </View>
+            <View style={styles.CenterContentWrapper}>
+              <Image
+                resizeMode="contain"
+                style={styles.BusinessImg}
+                source={require("../assets/Lines.png")}
+              />
+              <Text style={styles.DescriptionText}>
+                No history of outgoing transactions associated to your wallet
+                right now.
+              </Text>
+            </View>
+          </>
+        ) : null}
+
+        {outHistory.length > 0 ? (
+          <>
+            <View style={styles.wrapperTitle}>
+              <Title label="Payment history" />
+              <TouchableHighlight
+                underlayColor={"transparent"}
+                onPress={openShareModal}
+                activeOpacity={0.5}
+                style={styles.share}
+              >
+                <Image
+                  style={styles.shareImg}
+                  resizeMode="contain"
+                  source={require("../assets/share.png")}
+                />
+              </TouchableHighlight>
+            </View>
+            <View style={styles.nameParameters}>
+              <TextInput
+                // autoFocus={true}
+                numberOfLines={1}
+                placeholder="Enter your name"
+                value={userName}
+                maxLength={20}
+                onChangeText={changeUserName}
+                style={styles.Value}
+              />
+              <Image
+                style={styles.shareImg}
+                resizeMode="contain"
+                source={require("../assets/edit.png")}
               />
             </View>
             <GeneralPayInfo
               generalPayAmount={truncate(
                 spentTotalCryptoAmount,
-                BASE_CRYPTO_MAX_DIGITS,
+                BASE_CRYPTO_MAX_DIGITS
               )}
-              title={'Spent'}
+              title={"Spent"}
               generalPayAmountSubtitle={BASE_CRYPTO_CURRENCY}
-              TipsSubtitleLeft={'including'}
+              TipsSubtitleLeft={"including"}
               TipsAmount={truncate(
                 spentTipsCryptoAmount,
-                BASE_CRYPTO_MAX_DIGITS,
+                BASE_CRYPTO_MAX_DIGITS
               )}
-              TipsSubtitleRight={BASE_CRYPTO_CURRENCY + ' tips'}
+              TipsSubtitleRight={BASE_CRYPTO_CURRENCY + " tips"}
             />
 
             <FlatList
@@ -131,20 +165,20 @@ const InfoScreen = () => {
               onRefresh={loadMoreOutHistory}
               progressViewOffset={-90}
               data={outHistory}
-              keyExtractor={item => item.id}
-              renderItem={({item}) => (
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
                 <HistoryPay
                   time={new Date(item.timestamp * 1000).toISOString()}
                   company={item.businessId}
                   amount={
-                    '-' +
+                    "-" +
                     truncate(item.totalCryptoAmount, BASE_CRYPTO_MAX_DIGITS) +
-                    ' ' +
+                    " " +
                     BASE_CRYPTO_CURRENCY
                   }
                 />
               )}
-              ListFooterComponent={<View style={{height: 30}} />}
+              ListFooterComponent={<View style={{ height: 30 }} />}
             />
           </>
         ) : null}
@@ -162,125 +196,125 @@ const InfoScreen = () => {
 
 const styles = StyleSheet.create({
   InfoScreenWrapper: {
-    display: 'flex',
-    width: '100%',
-    height: '100%',
+    display: "flex",
+    width: "100%",
+    height: "100%",
     padding: 10,
     marginBottom: 60,
   },
   CenterContentWrapper: {
     flex: 1,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 100,
   },
   GeneralPay: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
     borderRadius: 4,
-    borderStyle: 'solid',
+    borderStyle: "solid",
     borderWidth: 1,
-    borderColor: '#0dd977',
+    borderColor: "#0dd977",
     marginBottom: 10,
     padding: 10,
   },
   GeneralPayLabel: {
-    fontWeight: '400',
+    fontWeight: "400",
     fontSize: 11,
     lineHeight: 13,
-    color: '#777777',
-    fontFamily: Platform.OS === 'ios' ? undefined : FontFamily.robotoRegular,
+    color: "#777777",
+    fontFamily: Platform.OS === "ios" ? undefined : FontFamily.robotoRegular,
   },
   AmountBlock: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
     height: 33,
     marginTop: 7,
     marginBottom: 7,
   },
   GeneralPayAmount: {
-    fontWeight: '700',
+    fontWeight: "700",
     fontSize: 28,
     lineHeight: 33,
-    color: '#222222',
-    fontFamily: Platform.OS === 'ios' ? undefined : FontFamily.robotoBold,
+    color: "#222222",
+    fontFamily: Platform.OS === "ios" ? undefined : FontFamily.robotoBold,
   },
   GeneralPayAmountSubtitle: {
-    fontWeight: '700',
+    fontWeight: "700",
     fontSize: 14,
     lineHeight: 17,
-    color: '#222222',
-    marginTop: 'auto',
+    color: "#222222",
+    marginTop: "auto",
     marginLeft: 5,
     marginBottom: 4,
-    fontFamily: Platform.OS === 'ios' ? undefined : FontFamily.robotoBold,
+    fontFamily: Platform.OS === "ios" ? undefined : FontFamily.robotoBold,
   },
   TipsBlock: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     height: 13,
   },
   TipsSubtitle: {
-    fontWeight: '400',
+    fontWeight: "400",
     fontSize: 11,
     lineHeight: 13,
-    color: '#777777',
-    fontFamily: Platform.OS === 'ios' ? undefined : FontFamily.robotoRegular,
+    color: "#777777",
+    fontFamily: Platform.OS === "ios" ? undefined : FontFamily.robotoRegular,
   },
   TipsAmount: {
-    fontWeight: '700',
+    fontWeight: "700",
     fontSize: 11,
     lineHeight: 13,
-    color: '#222222',
+    color: "#222222",
     marginLeft: 3,
     marginRight: 3,
-    fontFamily: Platform.OS === 'ios' ? undefined : FontFamily.robotoBold,
+    fontFamily: Platform.OS === "ios" ? undefined : FontFamily.robotoBold,
   },
   list: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    height: '100%',
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
     paddingLeft: 10,
     paddingRight: 10,
     borderRadius: 4,
-    borderStyle: 'solid',
+    borderStyle: "solid",
     borderWidth: 1,
-    borderColor: '#E3E3E3',
+    borderColor: "#E3E3E3",
     marginBottom: 70,
   },
   BusinessImg: {
     width: 174,
     height: 158,
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    marginLeft: "auto",
+    marginRight: "auto",
     marginTop: 10,
     marginBottom: 20,
   },
   DescriptionText: {
     fontSize: 12,
     lineHeight: 14,
-    color: '#919191',
-    textAlign: 'center',
+    color: "#919191",
+    textAlign: "center",
     paddingLeft: 20,
     paddingRight: 20,
     marginBottom: 20,
-    fontFamily: Platform.OS === 'ios' ? undefined : FontFamily.robotoRegular,
+    fontFamily: Platform.OS === "ios" ? undefined : FontFamily.robotoRegular,
   },
   wrapperTitle: {
-    display: 'flex',
-    width: '100%',
+    display: "flex",
+    width: "100%",
     paddingRight: 25,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    alignItems: 'center',
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
   },
   share: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     width: 24,
     height: 24,
   },
@@ -289,11 +323,11 @@ const styles = StyleSheet.create({
     height: 24,
   },
   nameParameters: {
-    display: 'flex',
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: '#fff',
+    display: "flex",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
     borderRadius: 4,
     padding: 10,
     marginBottom: 10,
@@ -301,14 +335,14 @@ const styles = StyleSheet.create({
   Value: {
     fontSize: 14,
     lineHeight: 16,
-    fontWeight: '400',
-    color: '#222222',
-    backgroundColor: '#fff',
+    fontWeight: "400",
+    color: "#222222",
+    backgroundColor: "#fff",
     padding: 0,
     margin: 0,
-    textAlign: 'left',
-    width: '80%',
-    fontFamily: Platform.OS === 'ios' ? undefined : FontFamily.robotoRegular,
+    textAlign: "left",
+    width: "80%",
+    fontFamily: Platform.OS === "ios" ? undefined : FontFamily.robotoRegular,
   },
 });
 
