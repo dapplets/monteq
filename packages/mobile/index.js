a020eba74f88fd6006acdae6685b8425b0f001ca
Alexander Sakhaev <alsakhaev@gmail.com>
2023-05-18 23:05:17 +0700

    build: platform specific code

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -1,9 +1,16 @@
-// import "react-native-reanimated";
-import './expo-crypto-shim.js' // Only for Expo SDK 48+
-import "react-native-url-polyfill/auto";
+import "./expo-crypto-shim.js"; // Only for Expo SDK 48+
+// import "react-native-url-polyfill/auto.js";
 import "@walletconnect/react-native-compat";
 import "@ethersproject/shims";
-import { registerRootComponent } from 'expo';
-import App from './src/App';
+import { registerRootComponent } from "expo";
+import App from "./src/App";
+import React from "react";
+import { Platform } from "react-native";
+
+if (Platform.OS === "web") {
+  window.React = React;
+} else {
+  require("react-native-url-polyfill/auto.js");
+}
 
 registerRootComponent(App);
