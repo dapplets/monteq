8453d70fc7aa454ff1851fbcd7c0aff6c2aabf28
Alexander Sakhaev <alsakhaev@gmail.com>
2023-05-25 15:30:19 +0700

    build: recover native android/ios build

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -1,17 +1,8 @@
-import "@ethersproject/shims"; // shims for ethers.js https://docs.ethers.org/v5/cookbook/react-native/#cookbook-reactnative
-// import "./expo-crypto-shim.js"; // Only for Expo SDK 48+
-// import "react-native-url-polyfill/auto.js";
-// import "@walletconnect/react-native-compat";
-// import "@ethersproject/shims";
-import { registerRootComponent } from "expo";
-import App from "./src/App";
-// import React from "react";
-// import { Platform } from "react-native";
+import './expo-crypto-shim.js'; // Only for Expo SDK 48+
+import '@ethersproject/shims'; // shims for ethers.js https://docs.ethers.org/v5/cookbook/react-native/#cookbook-reactnative
+import 'react-native-url-polyfill/auto.js'; // for URL parser
+import { registerRootComponent } from 'expo';
 
-// if (Platform.OS === "web") {
-//   window.React = React;
-// } else {
-//   // require("react-native-url-polyfill/auto.js");
-// }
+import App from './src/App';
 
 registerRootComponent(App);
